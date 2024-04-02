f0f8946763d3d8e4c96d0d63f3d1df9a07839288
Greg Caporaso <greg.caporaso@nau.edu>
2024-04-02 12:49:42 +0200

    add DNASequenceRecord semantic type...
    ... along with associated formats and a transformer

--- q2_dwq2/__init__.py
+++ q2_dwq2/__init__.py
@@ -9,8 +9,16 @@
 
 from ._version import get_versions
 
+from ._types_and_formats import (
+    SingleDNASequence, SingleRecordDNAFASTAFormat,
+    SingleRecordDNAFASTADirectoryFormat)
+
 __version__ = get_versions()["version"]
 del get_versions
 
 from . import _version
 __version__ = _version.get_versions()['version']
+
+__all__ = [
+    "SingleDNASequence", "SingleRecordDNAFASTAFormat",
+    "SingleRecordDNAFASTADirectoryFormat"]
