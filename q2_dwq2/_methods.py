dd7210a034bab9561d7a6c3d379c79b2b59a7462
Greg Caporaso <greg.caporaso@nau.edu>
2024-04-03 06:56:18 +0200

    adapt action with new type

--- q2_dwq2/_methods.py
+++ q2_dwq2/_methods.py
@@ -7,19 +7,15 @@
 # ----------------------------------------------------------------------------
 
 from skbio.alignment import global_pairwise_align_nucleotide, TabularMSA
+from skbio import DNA
 
-from q2_types.feature_data import DNAIterator
 
-
-def nw_align(seq1: DNAIterator,
-             seq2: DNAIterator,
+def nw_align(seq1: DNA,
+             seq2: DNA,
              gap_open_penalty: float = 5,
              gap_extend_penalty: float = 2,
              match_score: float = 1,
              mismatch_score: float = -2) -> TabularMSA:
-    seq1 = next(iter(seq1))
-    seq2 = next(iter(seq2))
-
     msa, _, _ = global_pairwise_align_nucleotide(
         seq1=seq1, seq2=seq2, gap_open_penalty=gap_open_penalty,
         gap_extend_penalty=gap_extend_penalty, match_score=match_score,
