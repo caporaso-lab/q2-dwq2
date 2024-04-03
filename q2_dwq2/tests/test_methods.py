a8fb087d82658fb89a05b3a6544620ef9bfa69ba
Greg Caporaso <greg.caporaso@nau.edu>
2024-04-03 06:56:18 +0200

    adapt action with new type

--- q2_dwq2/tests/test_methods.py
+++ q2_dwq2/tests/test_methods.py
@@ -11,9 +11,9 @@ from skbio.sequence import DNA
 
 from qiime2.plugin.testing import TestPluginBase
 from qiime2.plugin.util import transform
-from q2_types.feature_data import DNAFASTAFormat, DNAIterator
 
 from q2_dwq2._methods import nw_align
+from q2_dwq2._types_and_formats import SingleRecordDNAFASTAFormat
 
 
 class NWAlignTests(TestPluginBase):
@@ -22,9 +22,7 @@ class NWAlignTests(TestPluginBase):
     def test_simple1(self):
         # test alignment of a pair of sequences
         sequence1 = DNA('AAAAAAAAGGTGGCCTTTTTTTT')
-        sequence1 = DNAIterator([sequence1])
         sequence2 = DNA('AAAAAAAAGGGGCCTTTTTTTT')
-        sequence2 = DNAIterator([sequence2])
         observed = nw_align(sequence1, sequence2)
 
         aligned_sequence1 = DNA('AAAAAAAAGGTGGCCTTTTTTTT')
@@ -38,12 +36,12 @@ class NWAlignTests(TestPluginBase):
         # loaded from file this time, for demonstration purposes
         sequence1 = transform(
             self.get_data_path('seq-1.fasta'),
-            from_type=DNAFASTAFormat,
-            to_type=DNAIterator)
+            from_type=SingleRecordDNAFASTAFormat,
+            to_type=DNA)
         sequence2 = transform(
             self.get_data_path('seq-2.fasta'),
-            from_type=DNAFASTAFormat,
-            to_type=DNAIterator)
+            from_type=SingleRecordDNAFASTAFormat,
+            to_type=DNA)
         observed = nw_align(sequence1, sequence2)
 
         aligned_sequence1 = DNA('ACCGGTGGAACCGG-TAACACCCAC')
@@ -53,10 +51,8 @@ class NWAlignTests(TestPluginBase):
         self.assertNotEqual(observed, expected)
 
     def test_alt_match_score(self):
-        s1 = DNA('AAAATTT')
-        sequence1 = DNAIterator([s1])
-        s2 = DNA('AAAAGGTTT')
-        sequence2 = DNAIterator([s2])
+        sequence1 = DNA('AAAATTT')
+        sequence2 = DNA('AAAAGGTTT')
         # call with default value for match score
         observed = nw_align(sequence1, sequence2)
 
@@ -66,8 +62,6 @@ class NWAlignTests(TestPluginBase):
 
         self.assertEqual(observed, expected)
 
-        sequence1 = DNAIterator([s1])
-        sequence2 = DNAIterator([s2])
         # call with non-default value for match_score
         observed = nw_align(sequence1, sequence2, match_score=10)
 
@@ -86,10 +80,8 @@ class NWAlignTests(TestPluginBase):
         self.assertEqual(observed, expected)
 
     def test_alt_gap_open_penalty(self):
-        s1 = DNA('AAAATTT')
-        sequence1 = DNAIterator([s1])
-        s2 = DNA('AAAAGGTTT')
-        sequence2 = DNAIterator([s2])
+        sequence1 = DNA('AAAATTT')
+        sequence2 = DNA('AAAAGGTTT')
         observed = nw_align(sequence1, sequence2, gap_open_penalty=0.01)
 
         aligned_sequence1 = DNA('AAAA-T-TT-')
@@ -98,8 +90,6 @@ class NWAlignTests(TestPluginBase):
 
         self.assertEqual(observed, expected)
 
-        sequence1 = DNAIterator([s1])
-        sequence2 = DNAIterator([s2])
         observed = nw_align(sequence1, sequence2)
 
         aligned_sequence1 = DNA('--AAAATTT')
@@ -109,10 +99,8 @@ class NWAlignTests(TestPluginBase):
         self.assertEqual(observed, expected)
 
     def test_alt_gap_extend_penalty(self):
-        s1 = DNA('AAAATTT')
-        sequence1 = DNAIterator([s1])
-        s2 = DNA('AAAAGGTTT')
-        sequence2 = DNAIterator([s2])
+        sequence1 = DNA('AAAATTT')
+        sequence2 = DNA('AAAAGGTTT')
         observed = nw_align(sequence1, sequence2, gap_open_penalty=0.01)
 
         aligned_sequence1 = DNA('AAAA-T-TT-')
@@ -121,8 +109,6 @@ class NWAlignTests(TestPluginBase):
 
         self.assertEqual(observed, expected)
 
-        sequence1 = DNAIterator([s1])
-        sequence2 = DNAIterator([s2])
         observed = nw_align(sequence1, sequence2, gap_open_penalty=0.01,
                             gap_extend_penalty=0.001)
 
@@ -133,10 +119,8 @@ class NWAlignTests(TestPluginBase):
         self.assertEqual(observed, expected)
 
     def test_alt_mismatch_score(self):
-        s1 = DNA('AAAATTT')
-        sequence1 = DNAIterator([s1])
-        s2 = DNA('AAAAGGTTT')
-        sequence2 = DNAIterator([s2])
+        sequence1 = DNA('AAAATTT')
+        sequence2 = DNA('AAAAGGTTT')
         observed = nw_align(sequence1, sequence2, gap_open_penalty=0.01)
 
         aligned_sequence1 = DNA('AAAA-T-TT-')
@@ -145,8 +129,6 @@ class NWAlignTests(TestPluginBase):
 
         self.assertEqual(observed, expected)
 
-        sequence1 = DNAIterator([s1])
-        sequence2 = DNAIterator([s2])
         observed = nw_align(sequence1, sequence2, gap_open_penalty=0.1,
                             mismatch_score=-0.1)
 
