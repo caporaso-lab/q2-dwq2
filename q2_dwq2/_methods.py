843a3db9ea3dba9595cc47ec0f1d20c2c81ab487
Greg Caporaso <greg.caporaso@nau.edu>
2024-04-15 06:49:47 -0700

    add first pipeline

--- q2_dwq2/_methods.py
+++ q2_dwq2/_methods.py
@@ -9,13 +9,22 @@
 from skbio.alignment import global_pairwise_align_nucleotide, TabularMSA
 from skbio import DNA
 
+_nw_align_defaults = {
+    'gap_open_penalty': 5,
+    'gap_extend_penalty': 2,
+    'match_score': 1,
+    'mismatch_score': -2
+}
 
-def nw_align(seq1: DNA,
-             seq2: DNA,
-             gap_open_penalty: float = 5,
-             gap_extend_penalty: float = 2,
-             match_score: float = 1,
-             mismatch_score: float = -2) -> TabularMSA:
+
+def nw_align(
+        seq1: DNA,
+        seq2: DNA,
+        gap_open_penalty: float = _nw_align_defaults['gap_open_penalty'],
+        gap_extend_penalty: float = _nw_align_defaults['gap_extend_penalty'],
+        match_score: float = _nw_align_defaults['match_score'],
+        mismatch_score: float = _nw_align_defaults['mismatch_score']) \
+        -> TabularMSA:
     msa, _, _ = global_pairwise_align_nucleotide(
         seq1=seq1, seq2=seq2, gap_open_penalty=gap_open_penalty,
         gap_extend_penalty=gap_extend_penalty, match_score=match_score,
